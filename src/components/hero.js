c9bffababecf0a4282d2104c8de70a21963a5aa7
BirdyArt <asobolev91@gmail.com>
2021-02-20 15:56:06 -0800

    Dividers, Carousel adjusted

--- src/components/hero.js
+++ src/components/hero.js
@@ -24,32 +24,36 @@ function Hero(items) {
       <h2 className="intro" ref={(el) => (intro = el)}>{items.items[0].hero}</h2>
       <a id="fancyBtn"><svg><rect></rect></svg><span>{items.items[0].herobutton}</span></a>
       <Row className="author mx-auto">
+        <div className="text-divider">About Ashfaq</div>
         <Col xs={12} md={6} className="my-auto">
           <img src={authorImg} alt="" />
         </Col>
         <Col xs={12} md={6} className="my-auto">
           <div className="about">
-          <h3>About Ashfaq <a href="https://www.linkedin.com/in/ashfaqishaq/"><FontAwesomeIcon icon={["fab", "linkedin"]} size="2x" color="white" /></a></h3>
           <p>As founder and chair of the International Child Art Foundation, Ashfaq has examined and judged more children’s artwork than anyone else ever. He organizes the world’s largest school art program, produces the World Children’s Festival, and publishes the ChildArt quarterly. He is planting the seeds of freedom by fostering the next generation’s creativity and critical thinking. He inspires mutual empathy so that human diversity can amplify the fractal of collaborative innovation and self-discovery.</p>
-          <p>A multidisciplinary thinker, his writings have been published by the International Monetary Fund, Institutional Investor, Journal of Conflict Resolution, Dynamische Psychiatrie, and The Lancet. He pioneered STEAMS Education to integrate STEM disciplines with Art and Sport for imagination, resilience, and teamwork needed for a whole-learner approach. He opposes “childism”—the pervasive use by adults of labels such as childish, juvenile, or childlike. He is devoted to correct the language and the culture so that children have a voice in matters that shape the future—which belongs to them alone.</p>
+          <p>A multidisciplinary thinker, his writings have been published by the International Monetary Fund, Institutional Investor, Journal of Conflict Resolution, Dynamische Psychiatrie, and The Lancet. He pioneered STEAMS Education to integrate STEM disciplines with Art and Sport for imagination, resilience, and teamwork needed for a whole-learner approach.<br/><br/><a href="https://www.linkedin.com/in/ashfaqishaq/"> Connect with Ashfaq - <FontAwesomeIcon icon={["fab", "linkedin"]} size="lg" color="white" /></a></p>
           </div>
         </Col>
       </Row>
       <Row className="mx-auto">
+        <div className="text-divider">Testimonials</div>
         <Col xs={12} md={6} className="my-auto">
-          <h3>What people say</h3>
           <Carousel className="testimonials">
             <Carousel.Item>
-              <p>Ashfaq is a brilliant mover and shaker focused on world peace. By bringing the world's youth together quadrennially to Washington DC, often from nations in conflict, and using the universal language of art-making on the National Mall, he has converted their potential hostility to future peace-seeking world leadership. Ashfaq well deserves the many honors and awards he has received but the most flattering award would be using his model and copying his efforts in adult groups around the world. Imagine the possibilities as Ashfaq Ishaq does!</p>
+              <p>Ashfaq is a brilliant mover and shaker focused on world peace. Ashfaq well deserves the many honors and awards he has received but the most flattering award would be using his model and copying his efforts in adult groups around the world. Imagine the possibilities as Ashfaq Ishaq does!</p>
               <p>- Bobbi Stoll, Art Therapy Pioneer and Honorary Life Member of the Southern California Art Therapy Association</p>
             </Carousel.Item>
             <Carousel.Item>
-              <p>Ashfaq is a brilliant mover and shaker focused on world peace. By bringing the world's youth together quadrennially to Washington DC, often from nations in conflict, and using the universal language of art-making on the National Mall, he has converted their potential hostility to future peace-seeking world leadership. Ashfaq well deserves the many honors and awards he has received but the most flattering award would be using his model and copying his efforts in adult groups around the world. Imagine the possibilities as Ashfaq Ishaq does!</p>
-              <p>- Bobbi Stoll, Art Therapy Pioneer and Honorary Life Member of the Southern California Art Therapy Association</p>
+              <p>A true pioneer of vision and dedication Ashfaq has worked against many odds to bring a new understanding of the importance of the Arts and creativity to Governments educationalists parents and children alike. He deserves a Noble Peace prize for this achievement.</p>
+              <p>- Shona Hammond Boys, National Director at New Zealand Childrens Art House Foundation</p>
             </Carousel.Item>
             <Carousel.Item>
-              <p>Ashfaq is a brilliant mover and shaker focused on world peace. By bringing the world's youth together quadrennially to Washington DC, often from nations in conflict, and using the universal language of art-making on the National Mall, he has converted their potential hostility to future peace-seeking world leadership. Ashfaq well deserves the many honors and awards he has received but the most flattering award would be using his model and copying his efforts in adult groups around the world. Imagine the possibilities as Ashfaq Ishaq does!</p>
-              <p>- Bobbi Stoll, Art Therapy Pioneer and Honorary Life Member of the Southern California Art Therapy Association</p>
+              <p>Dr. Ishaq is the most respectable person I have ever met. He is a great leader, strategist, and a "policy artist" for creation. Working with Dr. Ishaq is truly a pleasure, and an invaluable learning process not only professionally, but also personally.</p>
+              <p>- Yuxuan Chen, Rhodes Scholar, University of Oxford</p>
+            </Carousel.Item>
+            <Carousel.Item>
+              <p>Ashfaq is an amazing creative man who is more than just a visionary. He is someone without limits or a "dis" comfort zone - he's at home wherever he happens to be because he knows that is where he's meant to be. Ashfaq has a down to earth and gentle soul that can coax hidden talents and the spirit of a child has never left this man among giants.</p>
+              <p>- Janet McElligott, President at McElligott International</p>
             </Carousel.Item>
           </Carousel>
         </Col>
