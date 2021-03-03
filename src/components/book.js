19926e123317badcdbd60234d846fd307ed08bd3
BirdyArt <asobolev91@gmail.com>
2021-03-02 17:54:57 -0800

    Added book cover

--- src/components/book.js
+++ src/components/book.js
@@ -1,5 +1,7 @@
 import React, { useEffect } from "react";
 import Jumbotron from 'react-bootstrap/Jumbotron';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import { Link as LinkRoute} from 'react-router-dom';
 import { Route } from 'react-router-dom';
 
@@ -13,14 +15,21 @@ function Book() {
 
   return (
     <>
-    <Jumbotron className="table text-center" style={{ backgroundImage: `linear-gradient(rgba(101,157,189, 0.3), rgba(101,157,189, 0.3)), url(${process.env.PUBLIC_URL + '/assets/header.png'})` }}>
-      <h4>Contents</h4>
-      <div className="tableOfContent"><LinkRoute to="/book/chapter1">Chapter 1. Double Rainbows and Obama</LinkRoute></div>
-      <div className="tableOfContent"><LinkRoute to="/book/chapter1">Chapter 2. Clinton and Charity</LinkRoute></div>
-      <div className="tableOfContent"><LinkRoute to="/book/chapter1">Chapter 3. Bush and Peacebuilding</LinkRoute></div>
-      <div className="tableOfContent"><LinkRoute to="/book/chapter1">Chapter 4. Fulbright and the Consort</LinkRoute></div>
-      <div className="tableOfContent"><LinkRoute to="/book/chapter1">Chapter 5. Dawn and Dusk</LinkRoute></div>
-      <div className="tableOfContent"><LinkRoute to="/book/chapter1">Index</LinkRoute></div>
+    <Jumbotron className="table" style={{ backgroundImage: `linear-gradient(rgba(101,157,189, 0.5), rgba(101,157,189, 0.5)), url(${process.env.PUBLIC_URL + '/assets/book.jpg'})` }}>
+      <h4 className="text-center">Contents</h4>
+      <Row>
+        <Col xs={12} md={5} className="my-auto text-center">
+          <img src={process.env.PUBLIC_URL + '/assets/cover.png'} alt="" />
+        </Col>
+        <Col xs={12} md={7} className="my-auto">
+          <div className="tableOfContent"><LinkRoute to="/book/chapter1">Chapter 1. Double Rainbows and Obama</LinkRoute></div>
+          <div className="tableOfContent"><LinkRoute to="/book/chapter1">Chapter 2. Clinton and Charity</LinkRoute></div>
+          <div className="tableOfContent"><LinkRoute to="/book/chapter1">Chapter 3. Bush and Peacebuilding</LinkRoute></div>
+          <div className="tableOfContent"><LinkRoute to="/book/chapter1">Chapter 4. Fulbright and the Consort</LinkRoute></div>
+          <div className="tableOfContent"><LinkRoute to="/book/chapter1">Chapter 5. Dawn and Dusk</LinkRoute></div>
+          <div className="tableOfContent"><LinkRoute to="/book/chapter1">Index</LinkRoute></div>
+        </Col>
+      </Row>
     </Jumbotron>
     <Route exact path="/book/chapter1" component={AppRoutes} />
     </>
