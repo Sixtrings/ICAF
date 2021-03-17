9558f8f6f2831623354c4a473e5f5b657378e9e7
BirdyArt <asobolev91@gmail.com>
2021-03-17 11:38:41 -0700

    Fixed margin on xl screens + footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Footer() {
   return (
     <div className="footer text-center"> 
-      <p>© Ashfaq Ishaq, Inc. 2021. Built with ❤️ by <a href="https://birdyart.ca/">BirdyArt</a></p>
+      <p>© Designed and built with ❤️ by <a href="https://birdyart.ca/">BirdyArt</a></p>
     </div>
   );
 }
